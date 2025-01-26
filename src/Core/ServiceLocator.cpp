aa244cb7647d54331753ed9b2d0038b203aa64bf
quin partridge <the.rageorsmash@gmail.com>
2025-01-26 23:09:25 +1300

    Minor fixes to imports

--- src/Core/ServiceLocator.cpp
+++ src/Core/ServiceLocator.cpp
@@ -1,22 +1,7 @@
-#include "ServiceLocator.hpp"
+#include "Core/ServiceLocator.hpp"
 
 ServiceLocator::ServiceLocator() {}
 
 ServiceLocator::~ServiceLocator() {}
 
 std::unordered_map<std::type_index, std::shared_ptr<void>> ServiceLocator::services;
-
-template <typename T>
-void ServiceLocator::Provide(std::shared_ptr<T> service) {
-    services[std::type_index(typeid(T))] = service;
-}
-
-template <typename T>
-T* ServiceLocator::Get() {
-    return static_cast<T*>(services[std::type_index(typeid(T))].get());
-}
-
-template <typename T>
-std::shared_ptr<T> ServiceLocator::GetShared() {
-    return std::static_pointer_cast<T>(services[std::type_index(typeid(T))]);
-}
